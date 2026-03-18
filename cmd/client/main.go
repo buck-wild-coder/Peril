eb4cee3c70f7163db6d667ef728c591018f78e15
Snip <185667147+snipwalter@users.noreply.github.com>
2026-03-18 08:20:47 -0400

    A: Decoupling

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	name, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+
+	url := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+	defer conn.Close()
+	fmt.Println("Connection was successful")
+
+	_, _, err = pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, routing.PauseKey+"."+name, routing.PauseKey, "Transient")
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+
+	gamelogic.PrintServerHelp()
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+
+		if words[0] == "pause" {
+			fmt.Println("sending a pause message")
+		} else if words[0] == "resume" {
+			fmt.Println("sending a resume message")
+		} else if words[0] == "quit" {
+			break
+		} else {
+
+		}
+	}
+
+	Exit()
+}
+
+func Exit() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("\nThe program is shutting down and close the connection.")
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	url := "amqp://guest:guest@localhost:5672/"
+
+	amqpConn, err := amqp.Dial(url)
+	if err != nil {
+		fmt.Println("error...", err)
+		return
+	}
+	defer amqpConn.Close()
+	fmt.Println("Connection was successful")
+
+	amqpChann, err := amqpConn.Channel()
+	if err != nil {
+		fmt.Println("Error occured..", err)
+		return
+	}
+	defer amqpChann.Close()
+
+	gamelogic.PrintServerHelp()
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+
+		switch words[0] {
+		case "pause":
+			fmt.Println("sending a pause message")
+			publish(amqpChann, true)
+		case "resume":
+			fmt.Println("sending a resume message")
+			publish(amqpChann, false)
+		case "quit":
+			fmt.Println("Quiting")
+			return
+		default:
+			fmt.Println("don't understand the command.")
+		}
+	}
+}
+
+func publish(amqpChann *amqp.Channel, IsPaused bool) {
+	err := pubsub.PublishJSON(amqpChann, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: IsPaused})
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 }
